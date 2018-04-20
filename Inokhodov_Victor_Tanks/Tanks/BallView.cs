a6ec860fd634cbdd75115d771027a539219cd94d
Victor Inokhodov <viktor.inohodov@gmail.com>
2018-04-20 17:49:08 +0300

    -

--- Inokhodov_Victor_Tanks/Tanks/BallView.cs
+++ Inokhodov_Victor_Tanks/Tanks/BallView.cs
@@ -12,8 +12,7 @@ namespace Tanks
     class BallView
     {
         public static void DrawBall(Ball ball, Graphics flagGraphics)
-        {
-<<<<<<< HEAD
+        { 
             if (ball.Bullet.IsEnable)
             {
                 var bul = ball.Bullet;
@@ -21,9 +20,7 @@ namespace Tanks
                 flagGraphics.FillEllipse(Brushes.Black, bul.PosX, bul.PosY, bul.Size, bul.Size);
             }
 
-=======
-            //flagGraphics.FillEllipse(Brushes.Blue, ball.PosX, ball.PosY, ball.Size, ball.Size);
->>>>>>> parent of 3bca169... finished Tanks game
+
             flagGraphics.DrawImage(ImageModel.GetImages().Ball, ball.PosX, ball.PosY, ball.Size, ball.Size);
         }
     }
